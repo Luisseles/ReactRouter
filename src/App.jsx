c904e1aa85ea7af3e511386b89cefd9582751f90
Luis Henrique <selesluishenrique@gmail.com>
2022-09-16 00:14:52 -0300

    :up:

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,18 @@
 import React from 'react'
-import { render } from 'react-dom'
+
 import './assets/css/base/base.css'
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom'
 
 import Home from './paginas/Home'
 import Sobre from './paginas/Sobre'
 import Pagina404 from './paginas/Pagina404'
+import Cabecalho from './components/Cabecalho'
 
 function App() {
 
   return (
     <Router>
+      <Cabecalho/>
       <Switch>
 
         <Route exact path={'/'}>
